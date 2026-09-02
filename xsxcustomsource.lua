3f2adc137977a936aa281a5d713c51478f70f77a
adadev-luau <lawalternativeemail@gmail.com>
2026-09-02 21:59:01 +0100

    Update xsxcustomsource.lua

--- xsxcustomsource.lua
+++ xsxcustomsource.lua
@@ -575,7 +575,7 @@ function library:InitNotifications(text, duration, callback)
         elseif type == "success" then
             bar.BackgroundColor3 = Color3.fromRGB(131, 255, 103)
         elseif type == "information" then
-            bar.BackgroundColor3 = Color3.fromRGB(126, 117, 255)
+            bar.BackgroundColor3 = Color3.fromRGB(128, 215, 255)
         end
     
         barCorner.CornerRadius = UDim.new(0, 2)
